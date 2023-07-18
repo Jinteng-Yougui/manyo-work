22d20b6a4cc7dd05ea1e170dbf2605c948f24792
yuki kondo <jinteng.yougui@gmail.com>
2023-07-18 18:09:46 +0900

    models task.rbをdescに変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :content, length: {in: 1..140}
-  scope :closest, -> {reorder("deadline ASC")}
+  scope :closest, -> {reorder("deadline DESC")}
   scope :search_by_title, -> (title){
     where("title like ?", "%#{title}%")
   }
