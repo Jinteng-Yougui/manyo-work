26864463f6cd175184fe71167b93cf078dd7938d
yuki kondo <jinteng.yougui@gmail.com>
2023-07-20 14:03:52 +0900

    applicationとsessions/newにloginが表示されている

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :login_required, only: [:new, :create]
+  skip_before_action :login_required, only: [:new, :create, :destroy]
   def new
   end
 
@@ -12,5 +12,11 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 end
