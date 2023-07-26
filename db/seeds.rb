e57d70460b1412e46f8e4d2d931d7893fe051885
yuki kondo <jinteng.yougui@gmail.com>
2023-07-26 15:31:23 +0900

    seed設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,34 +3,35 @@
 #
 # Examples:
 #
-  User.create(
-  name: 'yuki@kondo.com',
-  email: 'yuki@kondo.com',
-  password: 'yuki@kondo.com',
-  admin: 'true')
+  # User.create(
+  # name: 'yuki@kondo.com',
+  # email: 'yuki@kondo.com',
+  # password: 'yuki@kondo.com',
+  # admin: 'true')
 
   15.times do |user|
     User.create!(
-      name: "Dpro#{n+1}",
-      email: "dpro#{n+1}@dpro.com",
-      password: "Dpro#{n+1}"
-  end
-
-  priority_array = ["未着手","着手中", "完了"]
-  importance_array = ["高", "中", "低"]
-  15.times do |task|
-    Task.create!(
-      title: "Manyo#{n+1}",
-      content: "step#{n+1}"
-      deadline: "#{today+1}"
-      priority: "shuffled_priority_array.pop"
-      importance: "shuffled_importance_array.pop"
-      label:
+      name: "Dpro#{user+1}",
+      email: "dpro#{user+1}@dpro.com",
+      password: "Dpro#{user+1}"
     )
   end
 
   15.times do |label|
     Label.create!(
-      label_name: "#{課題+1}"
+      label_name: "#{label+1}"
+    )
+  end
+    
+  priority_array = ["未着手", "着手中", "完了"]
+  importance_array = ["高", "中", "低"]
+  15.times do |task|
+    Task.create!(
+      user_id: 1,
+      title: "Manyo#{task+1}",
+      content: "step#{task+1}",
+      deadline: "8/1/2023",
+      priority: priority_array.pop,
+      importance: 1
     )
   end
